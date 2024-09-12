209c856fec952e6ff479a6194cc9f9c1658bd2ac
NicholasDJM <11604027+NicholasDJM@users.noreply.github.com>
2024-09-12 15:19:07 -0400

    Prep for new Warbond Stratagems.

--- help/src/js/stratagems.js
+++ help/src/js/stratagems.js
@@ -13,7 +13,7 @@ const wiki = "https://helldivers.wiki.gg/images";
 export const version = "01.001.005",
 	/**
 	 * Complete list of all Stratagems currently available in Helldivers 2.
-	 * Each one has a `code` property, a series of direction inputs, which is used to generate Case statements in the AutoHotkey script.
+	 * Each one has a `code` property, a series of directional inputs, which is used to generate Case statements in the AutoHotkey script.
 	 * Some will have a display name, which should be used instead of the key.
 	 * ```ts
 	 * interface Stratagem {
@@ -28,14 +28,14 @@ export const version = "01.001.005",
 	 */
 	stratagems = [
 		// Weapons
-		{ key: "Machine Gun", type: ["weapon"], icon: wiki+"/e/e0/Machine_Gun_Stratagem_Icon.png", code: ["down","left","down","up","right"] },
+		{ key: "Machine Gun", type: ["weapon", "machine gun"], icon: wiki+"/e/e0/Machine_Gun_Stratagem_Icon.png", code: ["down","left","down","up","right"] },
 		{ key: "Anti-Material Rifle", type: ["weapon"], icon: wiki+"/3/3c/Anti-Materiel_Rifle_Stratagem_Icon.png", code: ["down","left","right","up","down"] },
 		{ key: "Stalwart", type: ["weapon","machine gun"], icon: wiki+"/4/46/Stalwart_Stratagem_Icon.png", code: ["down","left","down","up","up","left"] },
 		{ key: "Expendable Anti-Tank", type: ["weapon"], icon: wiki+"/1/1c/Expendable_Anti-Tank_Stratagem_Icon.png", code: ["down","down","left","up","right"] },
 		{ key: "Recoilless Rifle", type: ["weapon", "backpack", "team reload"], icon: wiki+"/7/70/Recoilless_Rifle_Stratagem_Icon.png", code: ["down","left","right","right","left"] },
 		{ key: "Flamethrower", type: ["weapon"], icon: wiki+"/7/75/Flamethrower_Stratagem_Icon.png", code: ["down","left","up","down","up"] },
 		{ key: "Autocannon", type: ["weapon", "backpack", "team reload"], icon: wiki+"/e/ef/Autocannon_Stratagem_Icon.png", code: ["down","left","down","up","up","right"] },
-		{ key: "Heavy Machine Gun", type: ["weapon"], icon: wiki+"/d/d9/Heavy_Machine_Gun_Stratagem_Icon.png", code: ["down","left","up","down","down"] },
+		{ key: "Heavy Machine Gun", type: ["weapon", "machine gun"], icon: wiki+"/d/d9/Heavy_Machine_Gun_Stratagem_Icon.png", code: ["down","left","up","down","down"] },
 		{ key: "Airburst Rocket Launcher", type: ["weapon", "backpack", "team reload"], icon: wiki+"/a/ad/RL-77_Airburst_Rocket_Launcher_Stratagem_Icon.png", code: ["down","up","up","left","right"] },
 		{ key: "Commando", type: ["weapon"], icon: wiki+"/7/78/Commando_Stratagem_Icon.png", code: ["down","left","up","down","right"] },
 		{ key: "Railgun", type: ["weapon"], icon: wiki+"/3/35/Railgun_Stratagem_Icon.png", code: ["down","right","down","up","left","right"] },
@@ -44,6 +44,9 @@ export const version = "01.001.005",
 		{ key: "Laser Cannon", type: ["weapon", "laser"], icon: wiki+"/c/c3/Laser_Cannon_Stratagem_Icon.png", code: ["down","left","down","up","left"] },
 		{ key: "Arc Thrower", type: ["weapon"], icon: wiki+"/1/10/Arc_Thrower_Stratagem_Icon.png", code: ["down","left","down","up","left","left"] },
 		{ key: "Quasar Cannon", type: ["weapon"], icon: wiki+"/8/87/Quasar_Cannon_Stratagem_Icon.png", code: ["down","down","up","left","right"] },
+		/* Upcoming in the new Warbond: Chemical Agents!
+		{ key: "Sterilizer", type: ["weapon"], code: ["down"]}
+		*/
 		// Orbitals
 		{ key: "Orbital Gatling Barrage", type: ["orbital"], icon: wiki+"/f/f6/Orbital_Gatling_Barrage_Stratagem_Icon.png", code: ["right","down","left","up","up"] },
 		{ key: "Orbital Airburst Strike", type: ["orbital"], icon: wiki+"/2/28/Orbital_Airburst_Strike_Stratagem_Icon.png", code: ["right","right","right"] },
@@ -68,23 +71,26 @@ export const version = "01.001.005",
 		// Backpacks
 		{ key: "Jump Pack", type: ["backpack"], icon: wiki+"/f/f5/Jump_Pack_Stratagem_Icon.png", code: ["down","up","up","down","up"] },
 		{ key: "Supply Pack", type: ["backpack"], icon: wiki+"/6/61/Supply_Pack_Stratagem_Icon.png", code: ["down","left","down","up","up","down"] },
-		{ key: "Guard Dog Rover", type: ["backpack","laser"], icon: wiki+"/6/6f/Guard_Dog_Rover_Stratagem_Icon.png", code: ["down","up","left","up","right","right"] },
-		{ key: "Guard Dog", type: ["backpack"], icon: wiki+"/7/73/Guard_Dog_Stratagem_Icon.png", code: ["down","up","left","up","right","down"] },
+		{ key: "Guard Dog Rover", type: ["backpack","laser", "guard dog"], icon: wiki+"/6/6f/Guard_Dog_Rover_Stratagem_Icon.png", code: ["down","up","left","up","right","right"], displayName: `"Guard Dog" Rover` },
+		{ key: "Guard Dog", type: ["backpack", "guard dog"], icon: wiki+"/7/73/Guard_Dog_Stratagem_Icon.png", code: ["down","up","left","up","right","down"], displayName: `"Guard Dog"` },
+		/* Upcoming in the new Warbond: Chemical Agents!
+		{ key: "Guard Dog Dog Breath", type: ["backpack", "guard dog"], code: ["down"], displayName: `"Guard Dog" Dog Breath`}
+		*/
 		{ key: "Ballistic Shield Backpack", type: ["backpack"], icon: wiki+"/3/37/Ballistic_Shield_Backpack_Stratagem_Icon.png", code: ["down","left","down","down","up","left"] },
 		{ key: "Shield Generator Pack", type: ["backpack"], icon: wiki+"/9/99/Shield_Generator_Pack_Stratagem_Icon.png", code: ["down","up","left","right","left","right"] },
 		// Defense
-		{ key: "Shield Generator Relay", type: ["defense"], icon: wiki+"/e/e4/Shield_Generator_Relay_Stratagem_Icon.png", code: ["down","down","left","right","left","right"] },
-		{ key: "Tesla Tower", type: ["defense"], icon: wiki+"/8/8f/Tesla_Tower_Stratagem_Icon.png", code: ["down","up","right","up","left","right"] },
-		{ key: "Anti-Personnel Minefield", type: ["defense"], icon: wiki+"/b/bb/Anti-Personnel_Minefield_Stratagem_Icon.png", code: ["down","left","up","right"] },
-		{ key: "Incendiary Mines", type: ["defense", "fire"], icon: wiki+"/a/a9/Incendiary_Minefield_Stratagem_Icon.png", code: ["down","left","left","down"] },
-		{ key: "Anti-Tank Mines", type: ["defense"], icon: wiki+"/b/ba/MD-17_Anti-Tank_Mines_Stratagem_Icon.png", code: ["down","left","up","up"] },
-		{ key: "HMG Emplacement", type: ["defense"], icon: wiki+"/0/03/HMG_Emplacement_Stratagem_Icon.png", code: ["down","up","left","right","right","left"] },
-		{ key: "Machine Gun Sentry", type: ["defense"], icon: wiki+"/5/5a/Machine_Gun_Sentry_Stratagem_Icon.png", code: ["down","up","right","right","up"] },
-		{ key: "Gatling Sentry", type: ["defense"], icon: wiki+"/2/28/Gatling_Sentry_Stratagem_Icon.png", code: ["down","up","right","left"] },
-		{ key: "Mortar Sentry", type: ["defense"], icon: wiki+"/a/ad/Mortar_Sentry_Stratagem_Icon.png", code: ["down","up","right","right","down"] },
-		{ key: "Autocannon Sentry", type: ["defense"], icon: wiki+"/a/a7/Autocannon_Sentry_Stratagem_Icon.png", code: ["down","up","right","up","left","up"] },
-		{ key: "Rocket Sentry", type: ["defense"], icon: wiki+"/6/62/Rocket_Sentry_Stratagem_Icon.png", code: ["down","up","right","right","left"] },
-		{ key: "EMS Mortar Sentry", type: ["defense"], icon: wiki+"/a/a8/AM-23_EMS_Mortar_Sentry_Stratagem_Icon.png", code: ["down","up","right","down","right"] },
+		{ key: "Shield Generator Relay", type: ["defense", "emplacement"], icon: wiki+"/e/e4/Shield_Generator_Relay_Stratagem_Icon.png", code: ["down","down","left","right","left","right"] },
+		{ key: "Tesla Tower", type: ["defense", "emplacement"], icon: wiki+"/8/8f/Tesla_Tower_Stratagem_Icon.png", code: ["down","up","right","up","left","right"] },
+		{ key: "Anti-Personnel Minefield", type: ["defense", "emplacement"], icon: wiki+"/b/bb/Anti-Personnel_Minefield_Stratagem_Icon.png", code: ["down","left","up","right"] },
+		{ key: "Incendiary Mines", type: ["defense", "fire", "emplacement"], icon: wiki+"/a/a9/Incendiary_Minefield_Stratagem_Icon.png", code: ["down","left","left","down"] },
+		{ key: "Anti-Tank Mines", type: ["defense", "emplacement"], icon: wiki+"/b/ba/MD-17_Anti-Tank_Mines_Stratagem_Icon.png", code: ["down","left","up","up"] },
+		{ key: "HMG Emplacement", type: ["defense", "emplacement"], icon: wiki+"/0/03/HMG_Emplacement_Stratagem_Icon.png", code: ["down","up","left","right","right","left"] },
+		{ key: "Machine Gun Sentry", type: ["defense", "sentry"], icon: wiki+"/5/5a/Machine_Gun_Sentry_Stratagem_Icon.png", code: ["down","up","right","right","up"] },
+		{ key: "Gatling Sentry", type: ["defense", "sentry"], icon: wiki+"/2/28/Gatling_Sentry_Stratagem_Icon.png", code: ["down","up","right","left"] },
+		{ key: "Mortar Sentry", type: ["defense", "sentry"], icon: wiki+"/a/ad/Mortar_Sentry_Stratagem_Icon.png", code: ["down","up","right","right","down"] },
+		{ key: "Autocannon Sentry", type: ["defense", "sentry"], icon: wiki+"/a/a7/Autocannon_Sentry_Stratagem_Icon.png", code: ["down","up","right","up","left","up"] },
+		{ key: "Rocket Sentry", type: ["defense", "sentry"], icon: wiki+"/6/62/Rocket_Sentry_Stratagem_Icon.png", code: ["down","up","right","right","left"] },
+		{ key: "EMS Mortar Sentry", type: ["defense", "sentry"], icon: wiki+"/a/a8/AM-23_EMS_Mortar_Sentry_Stratagem_Icon.png", code: ["down","up","right","down","right"] },
 		{ key: "Patriot Exosuit", type: ["vehicle", "mech"], icon: wiki+"/3/30/EXO-45_Patriot_Exosuit_Stratagem_Icon.png", code: ["left","down","right","up","left","down","down"] },
 		{ key: "Emancipator Exosuit", type: ["vehicle","mech"], icon: wiki+"/8/82/EXO-49_Emancipator_Exosuit_Stratagem_Icon.png", code: ["left","down","right","up","left","down","up"] },
 		// Mission
